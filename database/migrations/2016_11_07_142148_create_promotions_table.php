cecfa8028a1a9729907dc483fc83808b9997ea7f
milleniumce <pongaskm2007@gmail.com>
2016-11-07 21:53:10 +0700

    Add command to create promotionsTable
    - pls run php artisan migrate:reset
    - and run php artisan migrate
    enjoy it

---
+++ database/migrations/2016_11_07_142148_create_promotions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePromotionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('promotions', function (Blueprint $table) {
+          $table->increments('promotionID')->unique();
+          $table->string('promotionName');
+		  $table->longText('description');
+          $table->string('issueBy');
+		  $table->integer('value');
+          $table->date('expired');
+          $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('promotions');
+    }
+}
