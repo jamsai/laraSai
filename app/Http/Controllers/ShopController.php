254106fa0bd4a7934cf99dd773101d886a0454aa
milleniumce <pongaskm2007@gmail.com>
2016-11-09 00:39:17 +0700

    Add permission to shop controller
    shopkeeper can use this function only!

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class shopController extends Controller
 {
@@ -27,17 +29,28 @@ class shopController extends Controller
   public function addPointGET(){{
        //var_dump($_GET);
        //return ''.$_GET['customerID'];
-       $score = DB::table('users')->where('id', $_GET['customerID'])->value('score');
-       $name = DB::table('users')->where('id', $_GET['customerID'])->value('name');
-       $result = $score+$_GET['point'];
+       if (Auth::guest()) {
+         return 'pls login';
+       }
+       else{
+         $id = Auth::id();
+         $type = DB::table('users')->where('id', $id)->value('type');
+         if($type == 1){
+           return "You don't have permission!";
+         }
+         else{
+           $score = DB::table('users')->where('id', $_GET['customerID'])->value('score');
+           $name = DB::table('users')->where('id', $_GET['customerID'])->value('name');
+           $result = $score+$_GET['point'];
 
-       DB::table('users')
-             ->where('id', $_GET['customerID'])
-             ->update(['score' => $result]);
+           DB::table('users')
+                 ->where('id', $_GET['customerID'])
+                 ->update(['score' => $result]);
 
 
-      return $_GET['point'] .' add to ' . $_GET['customerID'] . ': ' . $name . ' : The result is :' . $result;
-
+          return $_GET['point'] .' add to ' . $_GET['customerID'] . ': ' . $name . ' : The result is :' . $result;
+         }
+       }
     }
   }
     public function remove($id, $point){{
@@ -58,17 +71,28 @@ class shopController extends Controller
     public function removePointGET(){{
        //var_dump($_GET);
        //return ''.$_GET['customerID'];
-       $score = DB::table('users')->where('id', $_GET['customerID'])->value('score');
-       $name = DB::table('users')->where('id', $_GET['customerID'])->value('name');
-       $result = $score-$_GET['point'];
+       if (Auth::guest()) {
+         return 'pls login';
+       }
+       else{
+         $id = Auth::id();
+         $type = DB::table('users')->where('id', $id)->value('type');
+         if($type == 1){
+           return "You don't have permission!";
+         }
+         else{
+           $score = DB::table('users')->where('id', $_GET['customerID'])->value('score');
+           $name = DB::table('users')->where('id', $_GET['customerID'])->value('name');
+           $result = $score-$_GET['point'];
 
-       DB::table('users')
-             ->where('id', $_GET['customerID'])
-             ->update(['score' => $result]);
+           DB::table('users')
+                 ->where('id', $_GET['customerID'])
+                 ->update(['score' => $result]);
+    
 
-
-      return $_GET['point'] .' remove from ' . $_GET['customerID'] . ': ' . $name . ' : The result is :' . $result;
-
+          return $_GET['point'] .' remove from ' . $_GET['customerID'] . ': ' . $name . ' : The result is :' . $result;
+         }
+       }
     }
   }
   public function createPromotion(){{
