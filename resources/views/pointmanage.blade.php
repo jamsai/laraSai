d812f78dd8eb32dec69ab8b174a84af9d84fcb24
milleniumce <pongaskm2007@gmail.com>
2016-11-08 17:55:35 +0700

    Add point management
    point management UI is now added!

---
+++ resources/views/pointmanage.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+  <form action='addPointGET' method='get'>
+    <label>
+      CustomerID:
+      <input = name='customerID' type='text'>
+    </label>
+    <br>
+    <label>
+      Point to add
+      <input = name='point' type='text'>
+    </label>
+    <br>
+
+    <input type='submit'>
+  </form>
+</body>
+</html>
