22afc58bcbc21e55d950fd85ff5b05efce7f757e
milleniumce <pongaskm2007@gmail.com>
2016-11-08 16:33:18 +0700

    PromotionController permission is added
    Customer cant add promotion!
    Note customer is 1
    shopkeeper is 2

--- app/Http/Controllers/PromotionController.php
+++ app/Http/Controllers/PromotionController.php
@@ -7,18 +7,32 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class PromotionController extends Controller
 {
     public function create(){{
         //return $_POST['username'];
-          DB::table('promotions')->insert([
-          'promotionName' => $_GET['promotionName'],
-          'description' => $_GET['description'],
-          'issueBy' => $_GET['issueBy'],
-          'expired' => $_GET['bday'],
-          'value' => $_GET['value']]);
-        return '555';
+
+        if (Auth::guest()) {
+          return 'pls login';
+        }
+        else {
+          $id = Auth::id();
+          $type = DB::table('users')->where('id', $id)->value('type');
+          if($type == 1){
+            return "You don't have permission!";
+          }
+          else{
+            DB::table('promotions')->insert([
+            'promotionName' => $_GET['promotionName'],
+            'description' => $_GET['description'],
+            'issueBy' => $_GET['issueBy'],
+            'expired' => $_GET['bday'],
+            'value' => $_GET['value']]);
+          return 'Add Complete';
+          }
+        }
 
     }
   }
