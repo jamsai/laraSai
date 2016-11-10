23244831cc8ae86c701ddaadf3bdd009c3bd5314
Zreast <zreast@hotmail.com>
2016-11-11 01:24:12 +0700

    edit Error Page title

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -2,7 +2,7 @@
 <html >
 <head>
   <meta charset="UTF-8">
-  <title>Fargo 404</title>
+  <title>Forbidden</title>
 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
 
