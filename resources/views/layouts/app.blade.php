61bb44427ae6c2e41d3ae65e9e6622334e2d6712
Zreast <zreast@hotmail.com>
2016-11-14 14:40:24 +0700

    fix favicon not show in server

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>My Jamsai</title>
-
+    <link rel="shortcut icon" href="assets/images/favicon.ico" />
     <!-- Styles -->
     <link rel='stylesheet prefetch' href='css/bootstrap.min.css'>
     <link rel='stylesheet prefetch' href='css/bootstrap-theme.min.css'>
